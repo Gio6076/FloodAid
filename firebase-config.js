92e34cb57cb5ab03114d0e384ce5602c12f5235e
Gio6076 <gebarola@gmail.com>
2025-11-14 00:23:57 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// firebase-config.js - COMPAT VERSION
+const firebaseConfig = {
+    apiKey: "AIzaSyBUPpAaIWbXWf0v3ummZnpWZWUssS-xrp4",
+    authDomain: "floodaid-59bda.firebaseapp.com",
+    projectId: "floodaid-59bda",
+    storageBucket: "floodaid-59bda.firebasestorage.app",
+    messagingSenderId: "104663586236",
+    appId: "1:104663586236:web:5d4e85e469eb5846a5c7b9",
+    measurementId: "G-WH8BSDBLMJ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize services
+// const auth = firebase.auth();
+// const db = firebase.firestore();
